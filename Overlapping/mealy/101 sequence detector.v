039436c20112a4b8130f1caf325d86d92b11bd67
Ravi Kumar <124238054+RaviKumarKorada217@users.noreply.github.com>
2023-03-12 15:05:10 +0530

    Create 101 sequence detector.v

---
+++ Overlapping/mealy/101 sequence detector.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//101 sequence detector mealy overlapping state
+module seq_det_101(clk,rstn,in,out);
+	input clk, rstn, in;
+	output out;
+		
+	parameter s0=0,s1=1,s2=2;
+	reg [1:0] cur_state, next_state;
+	
+	assign out=((in)&(cur_state==s2?1:0));
+	
+	always @ (posedge clk)
+	begin
+		if(!rstn)
+			cur_state<=s0;
+		else
+			cur_state <=next_state;
+	end
+	
+	always @ (cur_state or in) 
+	begin
+		case(cur_state)
+			s0: begin
+					if(in) next_state = s1;
+					else next_state=s0;
+				 end
+			s1: begin
+					if(in) next_state = s1;
+					else next_state=s2;
+				 end
+			s2: begin
+					if(in) next_state = s1;
+					else next_state=s0;
+				 end
+   	default: begin
+					next_state = s0;
+					end
+	  endcase
+	end
+	
+endmodule
